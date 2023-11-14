3df8566b75244406757fb47b0a7d8db4385de663
Caio Pellegrini <caiopellegrini97@gmail.com>
2023-11-14 15:45:25 -0300

    header quase 100% funcional

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
+    <!--
     <x-slot name="header">
-       <!-- <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('HOME') }}
-        </h2> -->
+        </h2> 
     </x-slot>
+    -->
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -11,6 +13,29 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("Você está logado!") }}
                 </div>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam sunt ab qui quaerat numquam autem fuga officia omnis corporis dolore? Exercitationem ut dolorem, animi incidunt doloremque earum quasi dolores suscipit.
+                </p>
             </div>
         </div>
     </div>
